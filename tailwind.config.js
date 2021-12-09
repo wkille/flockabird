7dd3f7edb8b66dda5d8900183c5d223ab9d03bde
Wayne Kille <wayne_kille@hotmail.com>
2021-12-09 15:37:43 +0000

    added banner to home (blog archive) page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-	purge: ['./public/**/*.html', './src/**/*.vue'],
+    mode: 'jit',
+	purge: [
+	  './public/**/*.html',
+	  './src/**/*.{js,jsx,ts,tsx,vue}',
+	],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		extend: {},
